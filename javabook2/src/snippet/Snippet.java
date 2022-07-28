92e7f7157df9bbfa98dcabba429b1556feb40eb8
anseungwoo <73145138+anseungwoo@users.noreply.github.com>
2022-07-28 09:18:17 +0900

    Delete javabook2 directory

--- javabook2/src/snippet/Snippet.java
+++
@@ -1,8 +0,0 @@
-package snippet;
-
-public class Snippet {
-	<c:set var="page" value="${(empty param.p)?1:param.p}"/>
-		<c:set var="startNum" value="${page-(page-1)%5}"/>
-		<c:set var="lastNum" value="${fn:substringBefore(Math.ceil(count/10),'.')}"/>
-}
-
